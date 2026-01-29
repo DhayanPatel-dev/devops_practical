20578c435fdd4a69be0205c35ec3c554cf64289f
DhayanPatel-dev <dhayanspatel_bscibm2025@iar.ac.in>
2026-01-29 19:28:03 +0530

    Implement user input and difference calculation
    Added user input for values a and b, and modified logic to compute their difference.

--- hello.py
+++ hello.py
@@ -1,1 +1,15 @@
 print("Hello, doing DevOps practical")
+
+a = int(input("Enter value of a : "))
+b = int(input("Enter value of b : "))
+
+if(a>b):
+    a = a-b
+    b = a+b
+
+    print(f"Value of a : {a} & b : {b}")
+else:
+    b = b-a
+    a = b+a
+
+    print(f"Value of a : {a} & b : {b}")   
